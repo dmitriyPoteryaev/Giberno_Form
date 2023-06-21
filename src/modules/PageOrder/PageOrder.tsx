cb9294b34ad8c2557dfd4bb790969de00881681e
dmitriyPoteryaev <dim.poteryaeff@yandex.ru>
2023-06-21 22:57:20 +0300

    fix api

--- src/modules/PageOrder/PageOrder.tsx
+++ src/modules/PageOrder/PageOrder.tsx
@@ -23,20 +23,27 @@ const PageOrder = observer(() => {
   const navigate = useNavigate();
   useEffect(() => {
     const curData: any = {
-      client_id: "fb1969e9-8fa1-4b40-a9a4-da10a3fd968e",
-      key_form: "58441a73-5fb8-479e-88c8-9472f8d02a1c",
+      client_id: "7bc05553-4b68-44e8-b7bc-37be63c6d9e9",
+      key_form: "497f6eca-6276-4993-bfeb-53cbbbba6f08",
     };
 
-    // location.search.split("&").forEach((line, i) => {
-    //   if (i === 0) {
-    //     curData.client_id =
-    //       line.split("=")[1] || "7bc05553-4b68-44e8-b7bc-37be63c6d9e9";
-    //   } else {
-    //     curData.key_form =
-    //       line.split("=")[1] || "497f6eca-6276-4993-bfeb-53cbbbba6f08";
-    //   }
+    location.search.split("&").forEach((line, i) => {
+      if (i === 0) {
+        curData.client_id =
+          line.split("=")[1] || "7bc05553-4b68-44e8-b7bc-37be63c6d9e9";
+      } else {
+        curData.key_form =
+          line.split("=")[1] || "497f6eca-6276-4993-bfeb-53cbbbba6f08";
+      }
+    });
 
-    // });
+    navigate(
+      "/formpay?client_id=" +
+        curData.client_id +
+        "&" +
+        "key_form=" +
+        curData.key_form
+    );
 
     navigate(
       "/formpay?client_id=" +
